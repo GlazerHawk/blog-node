b6dccbcb1a3d998067563e34a7abb12d2416cacb
mayk-hsm <mayk.hsmdeveloper@gmail.com>
2019-05-20 21:23:31 -0300

    Ajuste de miniatura de posts na página inicial, com adição de imagens

--- config/server.js
+++ config/server.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 app.set('assets', './app/assets');
 app.use('/assets', express.static('./app/assets'));
+app.use('/dbImages', express.static('./app/assets/dbImages'));
 
 app.listen(port,function(){
 	console.log('Servidor rodando com express na porta',port);
